0d98863abe6082efbf7e220410c6f1e16ed58ba2
JannikU09 <j.uphoff@innfactory.de>
2026-03-17 13:01:24 +0100

    Switch-case mit einem Array von Objekten ersetzt.

--- src/app/step/page.tsx
+++ src/app/step/page.tsx
@@ -1,36 +1,30 @@
 "use client"
 
+import React from "react";
 import { Step1 } from "@/src/components/step1/step1";
 import { Step2 } from "@/src/components/step2/step2";
 import { Step3 } from "@/src/components/step3/step3";
 import { Step4 } from "@/src/components/step4/step4";
 import { indexAtom } from "@/src/store/stateStore";
+import { FlowStep } from "@/src/utils/steps";
 import { useAtomValue } from "jotai";
 
 export default function Page() {
 
     const index = useAtomValue(indexAtom)
 
-    switch (index) {
-        case 0:
-            return (
-                <Step1 />
-            );
-        case 1:
-            return (
-                <Step2 />
-            );
-        case 2:
-            return (
-                <Step3 />
-            );
-        case 3:
-            return (
-                <Step4 />
-            );
-        default:
-            return (
-                <p>Zu großer Index</p>
-            )
-    }
+    const steps: FlowStep[] = [
+        { id: "Step1", component: <Step1 /> },
+        { id: "Step2", component: <Step2 /> },
+        { id: "Step3", component: <Step3 /> },
+        { id: "Step4", component: <Step4 /> },
+    ];
+
+    return (
+        <div>
+            <div>
+                {steps[index].component}
+            </div>
+        </div>
+    )
 }
