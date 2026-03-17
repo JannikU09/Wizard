73f79b3e53c5d3e1431b63621c99c1f1d341d89d
JannikU09 <j.uphoff@innfactory.de>
2026-03-17 08:37:30 +0100

    Redirect implementiert, um gleich den richtigen Pfad aufzurufen.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/step',
+        permanent: true
+      },
+    ];
+  },
+
+
+
   /* config options here */
 };
 
