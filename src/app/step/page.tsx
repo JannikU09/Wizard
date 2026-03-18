57d5c3183cbba87f6f94df6f7a8f7a28f968337c
JannikU09 <j.uphoff@innfactory.de>
2026-03-18 09:56:23 +0100

    MUI-Stepper implementiert, um den aktuellen Fortschritt anzuzeigen.

--- src/app/step/page.tsx
+++ src/app/step/page.tsx
@@ -8,19 +8,41 @@ import { indexAtom } from "@/src/store/stateStore";
 import { FlowStep } from "@/src/utils/steps";
 import { useAtomValue } from "jotai";
 
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+
 export default function Page() {
 
-    const index = useAtomValue(indexAtom)
+    const index = useAtomValue(indexAtom);
 
     const steps: FlowStep[] = [
-        { id: "Step1", component: <PersonalInfo /> },
-        { id: "Step2", component: <ResidenceInfo /> },
-        { id: "Step3", component: <ContactInfo /> },
-        { id: "Step4", component: <Summary /> },
+        { id: "Step1", component: <PersonalInfo />, label: "Personal Info" },
+        { id: "Step2", component: <ResidenceInfo />, label: "Residence Info" },
+        { id: "Step3", component: <ContactInfo />, label: "Contact Info" },
+        { id: "Step4", component: <Summary />, label: "Summary" },
     ];
 
     return (
-        <div>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+
+            <Box sx={{ width: "100%" }}>
+                <Stepper activeStep={index} alternativeLabel>
+                    {steps.map((step) => (
+                        <Step key={step.id}>
+                            <StepLabel>{step.label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Box>
             <div>
                 {steps[index].component}
             </div>
