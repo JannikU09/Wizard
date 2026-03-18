5e9298c6ea7efd41b8d148cd1d6b9c08508d9b4a
JannikU09 <j.uphoff@innfactory.de>
2026-03-18 09:54:34 +0100

    RootLayout, um Providers erweitert, um Design Überschreibungen zu verhindern.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Providers } from "../providers/providers";
 
 export const metadata: Metadata = {
   title: "Multi-Step Wizard",
@@ -23,12 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
-  );
+  )
 }
