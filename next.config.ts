606744d5a33a7b48f6190068f1e2959161344ab5
JannikU09 <j.uphoff@innfactory.de>
2026-03-18 09:49:09 +0100

    style: Code eingerückt.

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/step',
-        permanent: true
-      },
-    ];
-  },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/step',
+                permanent: true
+            },
+        ];
+    },
 };
 
 export default nextConfig;
